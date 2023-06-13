364930c05631aa8b70373f09e42269c68fe51e00
SylverQG <doublc_qluv@163.com>
2023-06-13 23:27:30 +0800

    configpy2

--- src/config.py
+++ src/config.py
@@ -10,31 +10,31 @@ archives_page_size = 30
 fetch_remote_imgs = False
 enable_jsdelivr = {
     "enabled": False,
-    "repo": "AlanDecode/Maverick@gh-pages"
+    "repo": "SylverQG/Blogs@gh-pages"
 }
 locale = "Asia/Shanghai"
 category_by_folder = False
 
 # For site
-site_name = "Maverick"
-site_logo = "${static_prefix}android-chrome-512x512.png"
-site_build_date = "2019-12-06T12:00+08:00"
-author = "AlanDecode"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
+# site_name = "Maverick"
+# site_logo = "${static_prefix}android-chrome-512x512.png"
+site_build_date = "2023-06-14T12:00+08:00"
+author = "SylverQG"
+email = "doublc_qluv@163.com"
+author_homepage = "https://SylverQG.github.io"
 description = "This is Maverick, Theme Galileo."
 key_words = ["Maverick", "AlanDecode", "Galileo", "blog"]
 language = 'english'
 external_links = [
     {
-        "name": "Maverick",
-        "url": "https://github.com/AlanDecode/Maverick",
-        "brief": "🏄‍ Go My Own Way."
+        "name": "SylverQG@Blogs",
+        "url": "https://github.com/SylverQG/Blogs",
+        "brief": "Put a ping."
     },
     {
-        "name": "Triple NULL",
-        "url": "https://www.imalan.cn",
-        "brief": "Home page for AlanDecode."
+        "name": "SylverQG@Blogs",
+        "url": "https://github.com/SylverQG/Blogs",
+        "brief": "THere is no spoon."
     }
 ]
 nav = [
@@ -58,17 +58,17 @@ nav = [
 social_links = [
     {
         "name": "Twitter",
-        "url": "https://twitter.com/AlanDecode",
+        "url": "https://twitter.com",
         "icon": "gi gi-twitter"
     },
     {
         "name": "GitHub",
-        "url": "https://github.com/AlanDecode",
+        "url": "https://github.com/SylverQG",
         "icon": "gi gi-github"
     },
     {
         "name": "Weibo",
-        "url": "https://weibo.com/5245109677/",
+        "url": "https://weibo.com",
         "icon": "gi gi-weibo"
     }
 ]
@@ -82,8 +82,8 @@ valine = {
     "recordIP": True
 }
 
-head_addon = ''
+head_addon = 'Put a ping'
 
 footer_addon = ''
 
-body_addon = ''
+body_addon = 'THere is no spoon'
